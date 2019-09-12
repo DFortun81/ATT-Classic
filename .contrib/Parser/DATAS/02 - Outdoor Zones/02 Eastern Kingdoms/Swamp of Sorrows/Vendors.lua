afed9810c8f81e1e4a73d255a4ed258777d7a33f
Crieve-Sargeras <dylanfortune@live.com>
2019-09-11 20:32:20 -0700

    Added the vendor Masat T'andr to Swamp of Sorrows.

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Swamp of Sorrows/Vendors.lua
@@ -0,0 +1,17 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(EASTERN_KINGDOMS, {	-- Eastern Kingdoms
+		m(SWAMP_OF_SORROWS, {	-- Swamp of Sorrows
+			n(-2, {	-- Vendor
+				n(11874, {	-- Masat T'andr <Superior Leatherworker>
+					i(15726),	-- Pattern: Green Dragonscale Breastplate
+					i(12253),	-- Brilliant Red Cloak
+					i(12254),	-- Well Oiled Cloak
+				}),
+			}),
+		}),
+	}),
+};
