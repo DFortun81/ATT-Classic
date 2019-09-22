96867ffb42b76c0a063fc100bb70d9355f544aa2
TKTalonzor <>
2019-09-22 11:35:42 +0300

    A vendor reached Wetlands

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Wetlands/Vendors.lua
@@ -0,0 +1,31 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(EASTERN_KINGDOMS, {	-- Eastern Kingdoms
+		m(WETLANDS, {	-- Wetlands
+			n(-2, {	-- Vendors
+				n(1454, {	-- Jennabink Powerseam <Tailoring Supplies & Specialty Goods>
+					["coord"] = { 8.1, 55.8, Wetlands },
+					["groups"] = {
+						i(4829),	-- Dreamer's Belt
+						i(4827),	-- Wizard's Belt
+						i(4828),	-- Nightwind Belt
+						i(4790),	-- Inferno Cloak
+						i(4793),	-- Sylvan Cloak
+						i(4792),	-- Spirit Cloak
+						i(5772),	-- Pattern: Red Woolen Bag
+						i(6275),	-- Pattern: Greater Adept's Robe
+					},
+				}),
+				n(1459, {	-- Naela Trance <Bowyer>
+					["coord"] = { 11.3, 58.4, Wetlands },
+					["groups"] = {
+						i(11304),	-- Fine Longbow
+					},
+				}),
+			}),
+		}),
+	}),
+};
