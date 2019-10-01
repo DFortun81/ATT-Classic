0cbed3799b5352d8a5ff17f4bd972bb0d7f54acf
mogwai316 <bryan.harden@gmail.com>
2019-09-30 20:20:52 -0400

    Added vendor to Burning Steppes (Deadly Scope schematic)

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Burning Steppes/Vendors.lua
@@ -0,0 +1,17 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(EASTERN_KINGDOMS, {	-- Eastern Kingdoms
+		m(BURNING_STEPPES, {	-- Blasted Lands
+			n(-2, {	-- Vendors
+				n(9544, {	-- Yuka Screwspigot
+					["groups"] = {
+						i(10602),	-- Schematic: Deadly Scope
+					},
+				}),				
+			}),	
+		}),
+	}),
+};
