185bcd0632f03df8c55f0c5621858c11086948d1
Peter Morgan <peter@1080peter.com>
2019-10-02 18:05:27 -0600

    Added coords to vendors in Teldrassil

--- .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Teldrassil/Vendors.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Teldrassil/Vendors.lua
@@ -7,12 +7,14 @@ _.Zones =
 		m(TELDRASSIL, {	-- Teldrassil
 			n(-2, {	-- Vendors
 				n(10118, {	-- Nessa Shadowsong <Fishing Supplies>
+					["coord"] = { 56.3, 92.4, TELDRASSIL },
 					["groups"] = {
 						i(6326),	-- Recipe: Slitherskin Mackerel
 						i(6368),	-- Recipe: Rainbow Fin Albacore				
 					},
 				}),
 				n(4265, {	-- Nyoma <Cooking Supplies>
+					["coord"] = { 57.2, 61.3, TELDRASSIL },
 					["groups"] = {
 						i(6325),	-- Recipe: Brilliant Smallfish
 						i(6328),	-- Recipe: Longjaw Mud Snapper
