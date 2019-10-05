661e05103619a866c4cd1c9fb5ec0349439a332f
mogwai316 <bryan.harden@gmail.com>
2019-10-05 15:32:17 -0400

    Added NYI section, added some more world and zone drop recipes

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Stranglethorn Vale/Zone Drops.lua
@@ -0,0 +1,15 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(EASTERN_KINGDOMS, {
+		m(STRANGLETHORN_VALE, {
+			n(0, {	-- Zone Drops
+				i(9294, {	-- Recipe: Wildvine Potion
+					["description"] = "Can drop from any troll in The Hinterlands or Stranglethorn Vale.",
+				}),
+			}),
+		}),
+	}),
+};
--- .contrib/Parser/DATAS/06 - World Drops/World Drops.lua
+++ .contrib/Parser/DATAS/06 - World Drops/World Drops.lua
@@ -5,6 +5,24 @@ _.WorldDrops =
 {
 	filter(200, {	-- Recipes
 		["g"] = bubbleDown({["collectible"] = true}, {
+			profession(171, {	-- Alchemy
+				i(3832),	-- Recipe: Elixir of Detect Lesser Invisibility
+				i(9297),	-- Recipe: Elixir of Dream Vision
+				i(3830),	-- Recipe: Elixir of Fortitude
+				i(9298),	-- Recipe: Elixir of Giants
+				i(3396),	-- Recipe: Elixir of Lesser Agility
+				i(2553),	-- Recipe: Elixir of Minor Agility
+				i(6211),	-- Recipe: Elixir of Ogre's Strength
+				i(3394),	-- Recipe: Elixir of Poison Resistance
+				i(3395),	-- Recipe: Limited Invulnerability Potion
+				i(3831),	-- Recipe: Mighty Troll's Blood Potion
+				i(2555),	-- Recipe: Swiftness Potion
+				i(9293),	-- Recipe: Magic Resistance Potion
+				i(3393),	-- Recipe: Minor Magic Resistance Potion
+			}),
+			profession(164, {	-- Blacksmithing
+				i(12713),	-- Plans: Radiant Leggings
+			}),
 			profession(202, {	-- Engineering
 				i(16055),	-- Schematic: Arcane Bomb
 				i(10601),	-- Schematic: Bright-Eye Goggles
@@ -17,6 +35,9 @@ _.WorldDrops =
 				i(10605),	-- Schematic: Spellpower Goggles Xtreme
 				i(16043),	-- Schematic: Thorium Rifle
 				i(16051),	-- Schematic: Thorium Shells
+			}),
+			profession(129, {	-- First Aid
+				i(6454),	-- Manual: Strong Anti-Venom
 			}),
 		}),
 	}),
