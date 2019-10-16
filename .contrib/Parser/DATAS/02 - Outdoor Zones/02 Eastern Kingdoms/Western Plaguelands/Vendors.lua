e6faa5131b1b26d7d364069c7404889c3c4da6a2
Crieve-Sargeras <dylanfortune@live.com>
2019-10-15 17:14:44 -0700

    Fixed an oopsie.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Western Plaguelands/Vendors.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Western Plaguelands/Vendors.lua
@@ -7,40 +7,44 @@ _.Zones =
 		m(WESTERN_PLAGUELANDS, {	-- Western Plaguelands
 			n(-2, {	-- Vendor
 				n(10857, {	-- Argent Quartermaster Lightspark <The Argent Dawn>
-					i(19447, {	-- Formula: Enchant Bracer - Healing
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(19446, {	-- Formula: Enchant Bracer - Mana Regeneration
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(19442, {	-- Formula: Powerful Anti-Venom
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(19216, {	-- Pattern: Argent Boots
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(19217, {	-- Pattern: Argent Shoulders
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(19328, {	-- Pattern: Dawn Treaders
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(19329, {	-- Pattern: Golden Mantle of the Dawn
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(19203, {	-- Plans: Girdle of the Dawn
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(19205, {	-- Plans: Gloves of the Dawn
-						["u"] = 3,	-- Added in later phase
-					}),
-					i(13482),	-- Recipe: Transmute Air to Fire
 					["coord"] = { 42.8, 83.7, WESTERN_PLAGUELANDS },
+					["groups"] = {
+						i(19447, {	-- Formula: Enchant Bracer - Healing
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(19446, {	-- Formula: Enchant Bracer - Mana Regeneration
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(19442, {	-- Formula: Powerful Anti-Venom
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(19216, {	-- Pattern: Argent Boots
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(19217, {	-- Pattern: Argent Shoulders
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(19328, {	-- Pattern: Dawn Treaders
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(19329, {	-- Pattern: Golden Mantle of the Dawn
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(19203, {	-- Plans: Girdle of the Dawn
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(19205, {	-- Plans: Gloves of the Dawn
+							["u"] = 3,	-- Added in later phase
+						}),
+						i(13482),	-- Recipe: Transmute Air to Fire
+					},
 				}),
 				n(12942, {	-- Leonard Porter <Leatherworking Supplies>
-					i(15725),	-- Pattern: Wicked Leather Gauntlets
-					i(15741),	-- Pattern: Stormshroud Pants
 					["coord"] = { 43.0, 84.3, WESTERN_PLAGUELANDS },
+					["groups"] = {
+						i(15725),	-- Pattern: Wicked Leather Gauntlets
+						i(15741),	-- Pattern: Stormshroud Pants
+					},
 				}),
 				n(11278, {	-- Magnus Frostwake
 					i(8030),	-- Plans: Ebon Shiv
