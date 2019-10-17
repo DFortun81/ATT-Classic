2c4d3b27b81b28c5c440e903f0702bbe5ecb0f9d
Peter Morgan <peter@1080peter.com>
2019-10-16 22:24:27 -0600

    Added cost to Burning Steppes "Libram of ___" quests

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Burning Steppes/Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Burning Steppes/Quests.lua
@@ -43,6 +43,13 @@ _.Zones =
 					["g"] = {
 						i(11642),	-- Lesser Arcanum of Constitution
 					},
+					["cost"] = {
+						{ "i", 11754, 1 }, -- Black Diamond
+						{ "i", 8411, 1 },  -- Lung Juice Cocktail
+						{ "i", 11733, 1 }, -- Libram of Constitution
+						{ "i", 11952, 4 }, -- Night Dragon's Breath
+						{ "g", 300000 },   -- 30g
+					},
 				}),
 				q(4483, {	-- Libram of Resilience
 					["lvl"] = 50,
@@ -50,6 +57,13 @@ _.Zones =
 					["qg"] = 9836,	-- Mathredis Firestar
 					["g"] = {
 						i(11644),	-- Lesser Arcanum of Resilience
+					},
+					["cost"] = {
+						{ "i", 11754, 1 }, -- Black Diamond
+						{ "i", 11567, 4 }, -- Crystal Spire
+						{ "i", 11751, 1 },  -- Bruning Essence
+						{ "i", 11736, 1 }, -- Libram of Resilience
+						{ "g", 300000 },   -- 30g
 					},
 				}),
 				q(4463, {	-- Libram of Rumination
@@ -59,6 +73,13 @@ _.Zones =
 					["g"] = {
 						i(11622),	-- Lesser Arcanum of Rumination
 					},
+					["cost"] = {
+						{ "i", 11754, 1 }, -- Black Diamond
+						{ "i", 11752, 1 }, -- Black Blood of the Tormented
+						{ "i", 8424, 1 },  -- Gizzard Gum
+						{ "i", 11732, 1 }, -- Libram of Rumination
+						{ "g", 300000 },   -- 30g
+					},
 				}),
 				q(4482, {	-- Libram of Tenacity
 					["lvl"] = 50,
@@ -66,6 +87,13 @@ _.Zones =
 					["qg"] = 9836,	-- Mathredis Firestar
 					["g"] = {
 						i(11643),	-- Lesser Arcanum of Tenacity
+					},
+					["cost"] = {
+						{ "i", 11754, 1 }, -- Black Diamond
+						{ "i", 11734, 1 }, -- Libram of Tenacity
+						{ "i", 11564, 4 }, -- Crystal Ward 
+						{ "i", 11753, 1 }, -- Eye of Kajal
+						{ "g", 300000 },   -- 30g
 					},
 				}),
 				q(4484, {	-- Libram of Voracity
@@ -78,6 +106,13 @@ _.Zones =
 						i(11649),	-- Lesser Arcanum of Voracity
 						i(11645),	-- Lesser Arcanum of Voracity
 						i(11646),	-- Lesser Arcanum of Voracity
+					},
+					["cost"] = {
+						{ "i", 11754, 1 }, -- Black Diamond
+						{ "i", 11737, 1 }, -- Libram of Voracity
+						{ "i", 11951, 4 }, -- Whipper Root Tuber
+						{ "i", 11563, 4 }, -- Crystal Force
+						{ "g", 300000 },   -- 30g
 					},
 				}),
 				q(3825, {	-- Ogre Head On A Stick = Party
