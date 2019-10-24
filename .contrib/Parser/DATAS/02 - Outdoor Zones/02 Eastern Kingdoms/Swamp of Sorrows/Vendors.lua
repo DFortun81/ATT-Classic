a7b9d07559531aa88c225e1622250d47ae99479d
Peter Morgan <peter@1080peter.com>
2019-10-23 21:07:15 -0600

    Added vendor coordinates: Swamp of Sorrow

--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Swamp of Sorrows/Vendors.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Swamp of Sorrows/Vendors.lua
@@ -15,9 +15,12 @@ _.Zones =
 					i(7995),	-- Plans: Mithril Scale Bracers
 				}),
 				n(11874, {	-- Masat T'andr <Superior Leatherworker>
-					i(15726),	-- Pattern: Green Dragonscale Breastplate
-					i(12253),	-- Brilliant Red Cloak
-					i(12254),	-- Well Oiled Cloak
+					["groups"] = {
+						i(15726),	-- Pattern: Green Dragonscale Breastplate
+						i(12253),	-- Brilliant Red Cloak
+						i(12254),	-- Well Oiled Cloak
+					},
+					["coord"] = { 26.5, 31.4, SWAMP_OF_SORROWS },
 				}),
 				n(8177, {	-- Rartar <Alchemy Supplies>
 					i(9300),	-- Recipe: Elixir of Demonslaying
