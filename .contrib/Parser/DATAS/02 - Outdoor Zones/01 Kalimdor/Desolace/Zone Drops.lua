ed7403cad2b24c13155a934e7ddcce5e8f27d2a5
Crieve-Sargeras <dylanfortune@live.com>
2019-10-30 19:42:12 -0700

    Added Augural Shroud as a Zone Drop of Desolace.

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Desolace/Zone Drops.lua
@@ -0,0 +1,15 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(KALIMDOR, {	-- Kalimdor
+		m(DESOLACE, {	-- Desolace
+			n(0, {	-- Zone Drops
+				i(2620, {	-- Augural Shroud
+					["cr"] = 4684,	-- Nether Sorceress
+				}),
+			}),
+		}),
+	}),
+};
