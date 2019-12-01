b1b7a8a2447eb0dee0d7c0cc34f0d75033634b6b
Crieve-Sargeras <dylanfortune@live.com>
2019-12-01 10:44:41 -0700

    Added Coordinates for Blimo and Jubie Gadgetspring.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Azshara/Vendors.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Azshara/Vendors.lua
@@ -7,19 +7,21 @@ _.Zones =
 		m(AZSHARA, {	-- Azshara
 			n(-2, {	-- Vendor
 				n(12957, {	-- Blimo Gadgetspring <Trade Supplier>
+					["coord"] = { 45.2, 90.8, AZSHARA },
 					["groups"] = {
 						i(15729),	-- Pattern: Chimeric Gloves
 						i(15751),	-- Pattern: Blue Dragonscale Breastplate
 					},
 				}),
 				n(14301, {	-- Brinna Valanaar <Bowyer>
+					["coord"] = { 12.0, 78.4, AZSHARA },
 					["races"] = ALLIANCE_ONLY,
 					["groups"] = {
 						i(11305),	-- Dense Shortbow
 					},
-					["coord"] = { 12.0, 78.4, AZSHARA },
 				}),
 				n(8678, {	-- Jubie Gadgetspring <Engineering Supplier>
+					["coord"] = { 45.2, 90.8, AZSHARA },
 					["groups"] = {
 						i(10607),	-- Schematic: Deepdive Helmet
 					},
