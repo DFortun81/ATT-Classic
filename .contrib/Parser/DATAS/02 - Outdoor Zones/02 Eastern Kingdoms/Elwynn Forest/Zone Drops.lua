7c42b35de3984fbf13dd260a364d6e3976c9d4e8
Crieve-Sargeras <dylanfortune@live.com>
2019-11-30 23:58:56 -0700

    Added a couple of zone drops.

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Elwynn Forest/Zone Drops.lua
@@ -0,0 +1,18 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(EASTERN_KINGDOMS, {	-- Eastern Kingdoms
+		m(ELWYNN_FOREST, {	-- Elwynn Forest
+			n(0, {	-- Zone Drop
+				n(471, {	-- Mother Fang
+					["coord"] = { 61.8, 47.8, ELWYNN_FOREST },
+					["groups"] = {
+						i(3000),	-- Brood Mother Carapace
+					},
+				}),
+			}),	
+		}),
+	}),
+};
---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Westfall/Zone Drops.lua
@@ -0,0 +1,34 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(EASTERN_KINGDOMS, {	-- Eastern Kingdoms
+		m(WESTFALL, {	-- Westfall
+			n(0, {	-- Zone Drop
+				n(519, {	-- Slark
+					["coords"] = {
+						{ 55.2, 13.4, WESTFALL },
+						{ 48.6, 10.0, WESTFALL },
+						{ 43.0, 10.0, WESTFALL },
+						{ 39.0, 15.0, WESTFALL },
+						{ 35.0, 20.6, WESTFALL },
+						{ 31.0, 26.4, WESTFALL },
+						{ 29.0, 34.6, WESTFALL },
+						{ 29.6, 39.8, WESTFALL },
+						{ 27.2, 45.0, WESTFALL },
+						{ 26.6, 56.4, WESTFALL },
+						{ 26.2, 65.6, WESTFALL },
+						{ 28.8, 72.6, WESTFALL },
+						{ 29.0, 79.4, WESTFALL },
+						{ 33.6, 83.2, WESTFALL },
+					},
+					["groups"] = {
+						i(3188),	-- Coral Claymore
+						i(6180),	-- Slarkskin
+					},
+				}),
+			}),	
+		}),
+	}),
+};
