e2021fd08860831f03624772525cc25ea4af30d8
Peter Morgan <peter@1080peter.com>
2020-01-01 15:00:20 -0700

    Added quests 8860 and 8861

---
+++ .contrib/Parser/DATAS/05 - Holidays/Misc.lua
@@ -0,0 +1,16 @@
+--------------------------------------------
+--     H O L I D A Y S  M O D U L E       --
+--------------------------------------------
+_.Holidays =
+{
+  n(-383, {	-- Miscellaneous
+    q(8860, { -- New Year Celebrations! (Alliance)
+      ["lvl"] = 1,
+      ["races"] = ALLIANCE_ONLY,
+    }),
+    q(8861, { -- New Year Celebrations! (Horde)
+      ["lvl"] = 1,
+      ["races"] = HORDE_ONLY,
+    }),
+  }),
+};
