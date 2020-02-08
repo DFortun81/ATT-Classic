d38cd432140e2254ded96bb04c8e6b9029bd114c
TKTalonzor <>
2020-02-08 08:40:37 +0200

    - Added DMF - work in progress

---
+++ .contrib/Parser/DATAS/05 - Holidays/05 - Darkmoon Faire/Quests.lua
@@ -0,0 +1,377 @@
+--------------------------------------------
+--     H O L I D A Y S  M O D U L E       --
+--------------------------------------------
+_.Holidays =
+{
+	m(407, {	-- Darkmoon Faire
+		n(-17, {	-- Quests
+			q(7885,  {	-- Armor Kits
+				["lvl"] = 40,
+				["qg"] = 14833,  -- Chronos <He Who Never Forgets!>
+				["cost"] = {
+					{ "i", 15564, 8 },	-- Rugged Armor Kit
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7892,  {	-- Big Black Mace
+				["lvl"] = 30,
+				["repeatable"] = true,
+				["qg"] = 14832,  -- Kerri Hicks <The Strongest Woman Alive!>
+				["cost"] = {
+					{ "i", 7945, 1 },	-- Big Black Mace
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7881,  {	-- Carnival Boots
+				["repeatable"] = true,
+				["qg"] = 14833,  -- Chronos <He Who Never Forgets!>
+				["cost"] = {
+					{ "i", 2309, 3 },	-- Embossed Leather Boots
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7882,  {	-- Carnival Jerkins
+				["lvl"] = 10,
+				["repeatable"] = true,
+				["qg"] = 14833,  -- Chronos <He Who Never Forgets!>
+				["cost"] = {
+					{ "i", 2314, 3 },	-- Toughened Leather Armor
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7889,  {	-- Coarse Weightstone
+				["repeatable"] = true,
+				["qg"] = 14832,  -- Kerri Hicks <The Strongest Woman Alive!>
+				["cost"] = {
+					{ "i", 3240, 10 },	-- Coarse Weightstone
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7894,  {	-- Copper Modulator
+				["repeatable"] = true,
+				["qg"] = 14841,  -- Rinling
+				["cost"] = {
+					{ "i", 4363, 5 },	-- Copper Modulator
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7884,  {	-- Crocolisk Boy and the Bearded Murloc
+				["lvl"] = 30,
+				["repeatable"] = true,
+				["qg"] = 14833,  -- Chronos <He Who Never Forgets!>
+				["cost"] = {
+					{ "i", 8185, 1 },	-- Turtle Scale Leggings
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7907,  {	-- Darkmoon Beast Deck
+				["provider"] = { "i", 19228 },  -- Beasts Deck
+				["cost"] = {
+					{ "i", 19228, 1 },	-- Beasts Deck
+				},
+				["g"] = {
+					i(19288),	-- Darkmoon Card: Blue Dragon
+				},
+			}),
+			q(7929,  {	-- Darkmoon Elementals Deck
+				["provider"] = { "i", 19267 },  -- Elementals Deck
+				["cost"] = {
+					{ "i", 19267, 1 },	-- Elementals Deck
+				},
+				["g"] = {
+					i(19289),	-- Darkmoon Card: Maelstrom
+				},
+			}),
+			q(7927,  {	-- Darkmoon Portals Deck
+				["provider"] = { "i", 19277 },  -- Portals Deck
+				["cost"] = {
+					{ "i", 19277, 1 },	-- Portals Deck
+				},
+				["g"] = {
+					i(19290),	-- Darkmoon Card: Twisting Nether
+				},
+			}),
+			q(7928,  {	-- Darkmoon Warlords Deck
+				["provider"] = { "i", 19257 },  -- Warlords Deck
+				["cost"] = {
+					{ "i", 19257, 1 },	-- Warlords Deck
+				},
+				["g"] = {
+					i(19287),	-- Darkmoon Card: Heroism
+				},
+			}),
+			q(7903,  {	-- Evil Bat Eyes
+				["lvl"] = 40,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["cost"] = {
+					{ "i", 11404, 10 },	-- Evil Bat Eye
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(8222,  {	-- Glowing Scorpid Blood
+				["lvl"] = 40,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["cost"] = {
+					{ "i", 19933, 10 },	-- Glowing Scorpid Blood
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7896,  {	-- Green Fireworks
+				["lvl"] = 20,
+				["repeatable"] = true,
+				["qg"] = 14841,  -- Rinling
+				["cost"] = {
+					{ "i", 9313, 36 },	-- Green Firework
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7891,  {	-- Green Iron Bracers
+				["lvl"] = 20,
+				["repeatable"] = true,
+				["qg"] = 14832,  -- Kerri Hicks <The Strongest Woman Alive!>
+				["cost"] = {
+					{ "i", 3835, 3 },	-- Green Iron Bracers
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7890,  {	-- Heavy Grinding Stone
+				["lvl"] = 10,
+				["repeatable"] = true,
+				["qg"] = 14832,  -- Kerri Hicks <The Strongest Woman Alive!>
+				["cost"] = {
+					{ "i", 3486, 7 },	-- Heavy Grinding Stone
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7897,  {	-- Mechanical Repair Kits
+				["lvl"] = 30,
+				["repeatable"] = true,
+				["qg"] = 14841,  -- Rinling
+				["cost"] = {
+					{ "i", 11590, 6 },	-- Mechanical Repair Kit
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7941,  {	-- More Armor Kits
+				["lvl"] = 40,
+				["repeatable"] = true,
+				["qg"] = 14833,  -- Chronos <He Who Never Forgets!>
+				["sourceQuest"] = 7885,	-- Armor Kits
+				["cost"] = {
+					{ "i", 15564, 8 },	-- Rugged Armor Kit
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7943,  {	-- More Bat Eyes
+				["lvl"] = 40,
+				["repeatable"] = true,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["cost"] = {
+					{ "i", 11404, 10 },	-- Evil Bat Eye
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7939,  {	-- More Dense Grinding Stones
+				["lvl"] = 40,
+				["repeatable"] = true,
+				["qg"] = 14832,  -- Kerri Hicks <The Strongest Woman Alive!>
+				["sourceQuest"] = 7893,	-- Rituals of Strength
+				["cost"] = {
+					{ "i", 12644, 8 },	-- Dense Grinding Stone
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(8223,  {	-- More Glowing Scorpid Blood
+				["lvl"] = 40,
+				["repeatable"] = true,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["sourceQuest"] = 8222,	-- Glowing Scorpid Blood
+				["cost"] = {
+					{ "i", 19933, 10 },	-- Glowing Scorpid Blood
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7942,  {	-- More Thorium Widgets
+				["lvl"] = 40,
+				["repeatable"] = true,
+				["qg"] = 14841,  -- Rinling
+				["sourceQuest"] = 7898,	-- Thorium Widget
+				["cost"] = {
+					{ "i", 15994, 6 },	-- Thorium Widget
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7893,  {	-- Rituals of Strength
+				["lvl"] = 40,
+				["qg"] = 14832,  -- Kerri Hicks <The Strongest Woman Alive!>
+				["cost"] = {
+					{ "i", 12644, 8 },	-- Dense Grinding Stone
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7899,  {	-- Small Furry Paws
+				["repeatable"] = true,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["cost"] = {
+					{ "i", 5134, 5 },	-- Small Furry Paw
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7901,  {	-- Soft Bushy Tails
+				["lvl"] = 20,
+				["repeatable"] = true,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["cost"] = {
+					{ "i", 4582, 5 },	-- Soft Bushy Tail
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7946,  {	-- Spawn of Jubjub
+				["lvl"] = 10,
+				["qg"] = 14871,  -- Morja
+				["cost"] = {
+					{ "i", 11325, 2 },	-- Dark Iron Ale Mug
+				},
+				["g"] = {
+					i(19462),	-- Unhatched Jubling Egg
+				},
+			}),
+			q(7883,  {	-- The World's Largest Gnome!
+				["lvl"] = 20,
+				["repeatable"] = true,
+				["qg"] = 14833,  -- Chronos <He Who Never Forgets!>
+				["cost"] = {
+					{ "i", 5739, 3 },	-- Barbaric Harness
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7898,  {	-- Thorium Widgets
+				["lvl"] = 40,
+				["qg"] = 14841,  -- Rinling
+				["cost"] = {
+					{ "i", 15994, 6 },	-- Thorium Widget
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7900,  {	-- Torn Bear Pelts
+				["lvl"] = 10,
+				["repeatable"] = true,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["cost"] = {
+					{ "i", 11407, 5 },	-- Torn Bear Pelt
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7902,  {	-- Vibrant Plumes
+				["lvl"] = 30,
+				["repeatable"] = true,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["cost"] = {
+					{ "i", 5117, 5 },	-- Vibrant Plume
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7895,  {	-- Whirring Bronze Gizmo
+				["lvl"] = 10,
+				["repeatable"] = true,
+				["qg"] = 14829,  -- Yebb Neblegear
+				["cost"] = {
+					{ "i", 4375, 7 },	-- Whirring Bronze Gizmo
+				},
+				["g"] = {
+					i(19182),	-- Darkmoon Faire Prize Ticket
+				},
+			}),
+			q(7937,  {	-- Your Fortune Awaits You...
+				["lvl"] = 10,
+				["provider"] = { "i", 19423 },  -- Sayge's Fortune #23
+				["cost"] = {
+					{ "i", 19423, 1 },	-- Sayge's Fortune #23
+				},
+				["g"] = {
+					i(19425),	-- Mysterious Lockbox
+				},
+			}),
+			q(7938,  {	-- Your Fortune Awaits You...
+				["lvl"] = 10,
+				["provider"] = { "i", 19424 },  -- Sayge's Fortune #24
+				["cost"] = {
+					{ "i", 19424, 1 },	-- Sayge's Fortune #24
+				},
+				["g"] = {
+					i(19425),	-- Mysterious Lockbox
+				},
+			}),
+			q(7944,  {	-- Your Fortune Awaits You...
+				["lvl"] = 10,
+				["provider"] = { "i", 19443 },  -- Sayge's Fortune #25
+				["cost"] = {
+					{ "i", 19443, 1 },	-- Sayge's Fortune #25
+				},
+				["g"] = {
+					i(19425),	-- Mysterious Lockbox
+				},
+			}),
+			q(7945,  {	-- Your Fortune Awaits You...
+				["lvl"] = 10,
+				["provider"] = { "i", 19452 },  -- Sayge's Fortune #27
+				["cost"] = {
+					{ "i", 19452, 1 },	-- Sayge's Fortune #27
+				},
+				["g"] = {
+					i(19425),	-- Mysterious Lockbox
+				},
+			}),
+		}),
+	}),
+};
---
+++ .contrib/Parser/DATAS/05 - Holidays/05 - Darkmoon Faire/Vendors.lua
@@ -0,0 +1,32 @@
+--------------------------------------------
+--     H O L I D A Y S  M O D U L E       --
+--------------------------------------------
+_.Holidays =
+{
+	m(407, {	-- Darkmoon Island
+		n(-2, {		-- Vendors
+			["groups"] = {
+				n(14860, { -- Flik
+					["groups"] = {
+						i(11026),	-- Tree Frog Box
+						i(11027),	-- Wood Frog Box
+						i(18662),	-- Heavy Leather Ball
+					},
+				}),
+				n(14828, { -- Gelvas Grimegate <Darkmoon Faire Ticket Redemption>
+					["groups"] = {
+						i(19295),	-- Darkmoon Flower
+						i(19298),	-- Minor Darkmoon Prize
+						i(19292),	-- Last Month's Mutton
+						i(19297),	-- Lesser Darkmoon Prize
+						i(19296),	-- Greater Darkmoon Prize
+						i(19291),	-- Darkmoon Storage Box
+						i(19293),	-- Last Year's Mutton
+						i(19491),	-- Amulet of the Darkmoon
+						i(19426),	-- Orb of the Darkmoon
+					},
+				}),
+			},
+		}),
+	}),
+};
