41e55e459116f65ded55e0b7a613e3ebf656d3c6
Crieve-Sargeras <dylanfortune@live.com>
2020-02-09 00:05:46 -0700

    Slightly updated the formating of the New Year Celebrations! holiday quest.

--- .contrib/Parser/DATAS/05 - Holidays/Misc.lua
+++ .contrib/Parser/DATAS/05 - Holidays/Misc.lua
@@ -3,14 +3,14 @@
 --------------------------------------------
 _.Holidays =
 {
-  n(-383, {	-- Miscellaneous
-    q(8860, { -- New Year Celebrations! (Alliance)
-      ["lvl"] = 1,
-      ["races"] = ALLIANCE_ONLY,
-    }),
-    q(8861, { -- New Year Celebrations! (Horde)
-      ["lvl"] = 1,
-      ["races"] = HORDE_ONLY,
-    }),
-  }),
+	n(-383, {	-- Miscellaneous
+		{
+			["allianceQuestID"] = 8860,	-- New Year Celebrations!
+			["hordeQuestID"] = 8861,	-- New Year Celebrations!
+			["qg"] = 15732,	-- Wonderform Operator <Smokywood Pastures>
+			["cost"] = {
+				{ "i", 21545, 1 },	-- Smokywood Supplies
+			},
+		},
+	}),
 };
