d68e57c9e0c126ca0281a7c89fbb79b6da2d6251
Crieve-Sargeras <dylanfortune@live.com>
2020-02-22 20:24:06 -0700

    Added all of the Rares to Dun Morogh, even those without any specific loot.

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Dun Morogh/Rares.lua
@@ -0,0 +1,81 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(EASTERN_KINGDOMS, {
+		m(DUN_MOROGH, {
+			n(RARES, {
+				n(1130, {	-- Bjarn
+					["coords"] = {
+						{ 52.8, 58.4, DUN_MOROGH },
+						{ 56.8, 56.8, DUN_MOROGH },
+						{ 63.6, 60.6, DUN_MOROGH },
+						{ 59.0, 61.6, DUN_MOROGH },
+					},
+					["groups"] = {
+						i(2069),	-- Black Bear Hide Vest
+					},
+				}),
+				n(1137, {	-- Edan the Howler
+					["coords"] = {
+						{ 43.6, 49.6, DUN_MOROGH },
+						{ 39.6, 48.2, DUN_MOROGH },
+						{ 42.2, 46.4, DUN_MOROGH },
+					},
+					["groups"] = {
+						i(3225),	-- Bloodstained Knife
+						i(3008),	-- Wendigo Fur Cloak
+					},
+				}),
+				n(8503, {	-- Gibblewilt
+					["coords"] = {
+						{ 27.2, 36.6, DUN_MOROGH },
+						{ 25.0, 44.6, DUN_MOROGH },
+					},
+					["groups"] = {
+						i(10554),	-- Foreman Pants
+						i(10553),	-- Foreman Vest
+					},
+				}),
+				n(1260, {	-- Great Father Arctikus
+					["coords"] = {
+						{ 23.8, 53.4, DUN_MOROGH },
+						{ 22.0, 51.0, DUN_MOROGH },
+					},
+					["groups"] = {
+						i(3223),	-- Frostmane Scepter
+						i(2546),	-- Royal Frostmane Girdle
+					},
+				}),
+				n(1119, {	-- Hammerspine
+					["coords"] = {
+						{ 71.8, 51.4, DUN_MOROGH },
+						{ 72.8, 53.8, DUN_MOROGH },
+					},
+					["groups"] = {
+						i(763),		-- Ice-covered Bracers
+						i(2254),	-- Icepane Warhammer
+					},
+				}),
+				n(1271, {	-- Old Icebeard
+					["coord"] = { 38.4, 54.0, DUN_MOROGH },
+					["groups"] = {
+						i(2899),	-- Wengido Collar
+					},
+				}),
+				n(1132, {	-- Timber
+					["coords"] = {
+						{ 36.0, 37.8, DUN_MOROGH },
+						{ 35.6, 42.8, DUN_MOROGH },
+						{ 31.8, 42.6, DUN_MOROGH },
+					},
+					["groups"] = {
+						i(3224),	-- Silver-lined Bracers
+						i(1965),	-- White Wolf Gloves
+					},
+				}),
+			}),
+		}),
+	}),
+};
--- .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Dun Morogh/Zone Drops.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/02 Eastern Kingdoms/Dun Morogh/Zone Drops.lua
@@ -6,18 +6,6 @@ _.Zones =
 	m(EASTERN_KINGDOMS, {
 		m(DUN_MOROGH, {
 			n(ZONEDROPS, {
-				i(2069, {	-- Black Bear Hide Vest
-					["cr"] = 1130,	-- Bjarn
-				}),
-				i(3225, {	-- Bloodstained Knife
-					["cr"] = 1137,	-- Edan the Howler
-				}),
-				i(10554, {	-- Foreman Pants
-					["cr"] = 8503,	-- Gibblewilt
-				}),
-				i(10553, {	-- Foreman Vest
-					["cr"] = 8503,	-- Gibblewilt
-				}),
 				i(2259, {	-- Frostmane Club
 					["cr"] = 1121,	-- Frostmane Snowstrider
 				}),
@@ -30,35 +18,14 @@ _.Zones =
 				i(2108, {	-- Frostmane Leather Vest
 					["cr"] = 706,	-- Frostmane Troll Whelp
 				}),
-				i(3223, {	-- Frostmane Scepter
-					["cr"] = 1260,	-- Great Father Arctikus
-				}),
-				i(763, {	-- Ice-covered Bracers
-					["cr"] = 1119,	-- Hammerspine
-				}),
-				i(2254, {	-- Icepane Warhammer
-					["cr"] = 1119,	-- Hammerspine
-				}),
 				i(2898, {	-- Mountaineer Chestpiece
 					["cr"] = 1196,	-- Ice Claw Bear
-				}),
-				i(2546, {	-- Royal Frostmane Girdle
-					["cr"] = 1260,	-- Great Father Arctikus
 				}),
 				i(2066, {	-- Skull Hatchet
 					["cr"] = 1115,	-- Rockjaw Skullthumper
 				}),
-				i(3224, {	-- Silver-lined Bracers
-					["cr"] = 1132,	-- Timber
-				}),
 				i(2787, {	-- Trogg Dagger
 					["cr"] = 724,	-- Burly Rockjaw Trogg
-				}),
-				i(2899, {	-- Wengido Collar
-					["cr"] = 1271,	-- Old Icebeard
-				}),
-				i(3008, {	-- Wendigo Fur Cloak
-					["cr"] = 1137,	-- Edan the Howler
 				}),
 				i(2671, {	-- Wendigo Mane
 					["questID"] = 313,	-- The Grizzled Den
@@ -68,9 +35,6 @@ _.Zones =
 						1137,	-- Edan the Howler
 						1135,	-- Wendigo
 					},
-				}),
-				i(1965, {	-- White Wolf Gloves
-					["cr"] = 1132,	-- Timber
 				}),
 			}),
 		}),
