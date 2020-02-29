0acbf0836b71b478c3400332c05a3ac58982393b
Crieve-Sargeras <dylanfortune@live.com>
2020-02-29 10:59:13 -0700

    Added Rares to Dustwallow Marsh.

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/01 Kalimdor/Dustwallow Marsh/Rares.lua
@@ -0,0 +1,80 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(KALIMDOR, {
+		m(DUSTWALLOW_MARSH, {
+			n(RARES, {
+				n(4339, {	-- Brimgore
+					["coords"] = {
+						{ 41.2, 75.2, DUSTWALLOW_MARSH },
+						{ 43.6, 77.4, DUSTWALLOW_MARSH },
+						{ 45.2, 81.8, DUSTWALLOW_MARSH },
+					},
+				}),
+				n(14230, {	-- Burgle Eye
+					["coords"] = {
+						{ 63.0, 7.2, DUSTWALLOW_MARSH },
+						{ 59.6, 10.6, DUSTWALLOW_MARSH },
+						{ 57.6, 17.6, DUSTWALLOW_MARSH },
+						{ 62.8, 18.4, DUSTWALLOW_MARSH },
+						{ 64.4, 28.6, DUSTWALLOW_MARSH },
+					},
+				}),
+				n(4380, {	-- Darkmist Widow
+					["coord"] = { 31.0, 20.6, DUSTWALLOW_MARSH },
+				}),
+				n(14232, {	-- Dart
+					["coord"] = { 47.6, 18.6, DUSTWALLOW_MARSH },
+				}),
+				n(14231, {	-- Drogoth the Roamer
+					["coords"] = {
+						{ 36.2, 13.6, DUSTWALLOW_MARSH },
+						{ 39.4, 14.4, DUSTWALLOW_MARSH },
+						{ 39.6, 21.2, DUSTWALLOW_MARSH },
+						{ 42.2, 18.8, DUSTWALLOW_MARSH },
+						{ 42.8, 22.0, DUSTWALLOW_MARSH },
+					},
+				}),
+				n(14234, {	-- Hayoc
+					["coords"] = {
+						{ 54.8, 60.0, DUSTWALLOW_MARSH },
+						{ 52.0, 63.6, DUSTWALLOW_MARSH },
+						{ 52.0, 65.4, DUSTWALLOW_MARSH },
+						{ 53.6, 66.2, DUSTWALLOW_MARSH },
+						{ 55.6, 67.8, DUSTWALLOW_MARSH },
+					},
+				}),
+				n(14236, {	-- Lord Angler
+					["coord"] = { 55.8, 64.6, DUSTWALLOW_MARSH },
+				}),
+				n(14237, {	-- Oozeworm
+					["coords"] = {
+						{ 39.8, 62.6, DUSTWALLOW_MARSH },
+						{ 36.6, 62.6, DUSTWALLOW_MARSH },
+						{ 36.0, 68.4, DUSTWALLOW_MARSH },
+					},
+				}),
+				n(14233, {	-- Ripscale
+					["coords"] = {
+						{ 37.8, 50.4, DUSTWALLOW_MARSH },
+						{ 43.8, 50.2, DUSTWALLOW_MARSH },
+						{ 42.6, 55.4, DUSTWALLOW_MARSH },
+						{ 47.6, 54.8, DUSTWALLOW_MARSH },
+						{ 50.0, 57.6, DUSTWALLOW_MARSH },
+					},
+				}),
+				n(14235, {	-- The Rot
+					["coords"] = {
+						{ 51.2, 50.8, DUSTWALLOW_MARSH },
+						{ 52.6, 51.6, DUSTWALLOW_MARSH },
+						{ 53.6, 54.2, DUSTWALLOW_MARSH },
+						{ 52.2, 57.4, DUSTWALLOW_MARSH },
+						{ 52.0, 60.4, DUSTWALLOW_MARSH },
+					},
+				}),
+			}),
+		}),
+	}),
+};
