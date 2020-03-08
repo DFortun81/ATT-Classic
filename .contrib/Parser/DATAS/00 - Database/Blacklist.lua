d5d571eca8f8e73e5bc2177061fb8b7368e9154e
Crieve-Sargeras <dylanfortune@live.com>
2020-03-07 23:45:11 -0700

    There's now a section for contribs to mark items as hidden from the unsorted list.

---
+++ .contrib/Parser/DATAS/00 - Database/Blacklist.lua
@@ -0,0 +1,53 @@
+---------------------------------------
+--        B L A C K L I S T          --
+---------------------------------------
+_.Blacklist =
+{
+	-- Bind Type 0
+	-- Invalid
+	i(7909),	-- Aquamarine
+	i(12800),	-- Azerothian Diamond
+	i(13468),	-- Black Lotus
+	i(7971),	-- Black Pearl
+	i(12361),	-- Blue Sapphire
+	i(7082),	-- Essence of Air
+	i(7076),	-- Essence of Earth
+	i(12808),	-- Essence of Undeath
+	i(7080),	-- Essence of Water
+	i(5760),	-- Eternium Lockbox
+	i(13926),	-- Golden Pearl
+	i(11082),	-- Greater Astral Essence
+	i(10939),	-- Greater Magic Essence
+	i(11135),	-- Greater Mystic Essence
+	i(11175),	-- Greater Nether Essence
+	i(4633),	-- Heavy Bronze Lockbox
+	i(12364),	-- Huge Emerald
+	i(5500),	-- Iridescent Pearl
+	i(4634),	-- Iron Lockbox
+	i(11084),	-- Large Glimmering Shard
+	i(11139),	-- Large Glowing Shard
+	i(12799),	-- Large Opal
+	i(11178),	-- Large Radiant Shard
+	i(10998),	-- Lesser Astral Essence
+	i(16202),	-- Lesser Eternal Essence
+	i(10938),	-- Lesser Magic Essence
+	i(1705),	-- Lesser Moonstone
+	i(11134),	-- Lesser Mystic Essence
+	i(12803),	-- Living Essence
+	i(774),	-- Malachite
+	i(5758),	-- Mithril Lockbox
+	i(4632),	-- Ornate Bronze Lockbox
+	i(4638),	-- Reinforced Steel Lockbox
+	i(1210),	-- Shadowgem
+	i(2775),	-- Silver Ore
+	i(14343),	-- Small Brilliant Shard
+	i(10978),	-- Small Glimmering Shard
+	i(11138),	-- Small Glowing Shard
+	i(5498),	-- Small Lustrous Pearl
+	i(11177),	-- Small Radiant Shard
+	i(4637),	-- Steel Lockbox
+	i(4636),	-- Strong Iron Lockbox
+	i(5759),	-- Thorium Lockbox
+	i(818),	-- Tigerseye
+	i(7911),	-- Truesilver Ore
+};
