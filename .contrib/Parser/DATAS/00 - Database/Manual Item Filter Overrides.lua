ee23984016d0ee43123886ef6172159c6073f358
Crieve <dylanfortune@live.com>
2021-02-09 17:58:59 -0700

    Fixed a number of filtering issues involving One-Handed Axes and Off-Hand Weapons.

--- .contrib/Parser/DATAS/00 - Database/Manual Item Filter Overrides.lua
+++ .contrib/Parser/DATAS/00 - Database/Manual Item Filter Overrides.lua
@@ -4,5 +4,30 @@ local i = function(itemID, filterID)
 	return o;
 end
 
--- Classic
-i(18202, 34);	-- Eskhandar's Left Claw [Fist Weapon]
+-- Axes
+i(9608, 21);	-- Shoni's Disarming Tool
+i(12502, 21);	-- Monster - Glaive - Demonhunter Black Offhand
+
+-- Daggers
+i(3865, 20);	-- Test Offhand Weapon
+i(18392, 20);	-- Distracting Dagger
+
+-- Swords
+i(1018, 25);	-- Chows Blade of DOOM! (Test)
+i(12939, 25);	-- Dal'Rend's Tribal Guardian
+i(19866, 25);	-- Warblade of the Hakkari
+
+-- Fist Weapons
+i(19910, 34);	-- Arlokk's Grasp
+i(17738, 34);	-- Claw of Celebras
+i(23242, 34);	-- Claw of the Frost Wyrm
+i(18202, 34);	-- Eskhandar's Left Claw
+i(13399, 34);	-- Gargoyle Shredder Talons
+i(18847, 34);	-- Grand Marshal's Left Hand Blade
+i(18848, 34);	-- High Warlord's Left Claw
+i(15909, 34);	-- Left-Handed Blades
+i(15906, 34);	-- Left-Handed Brass Knuckles
+i(15907, 34);	-- Left-Handed Claw
+i(11505, 34);	-- Monster - Claw - Bear Offhand
+i(11506, 34);	-- Monster - Claw Offhand
+i(11863, 34);	-- White Bone Shredder
