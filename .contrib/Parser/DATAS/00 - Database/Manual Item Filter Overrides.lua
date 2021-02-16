5ccaad9010a34393a0fcbf129ea5a47c1b0bee02
Crieve <dylanfortune@live.com>
2021-02-16 14:23:36 -0700

    Assigned the Mount filter type to items that are mounts, rather than Invalid/Ignored.

--- .contrib/Parser/DATAS/00 - Database/Manual Item Filter Overrides.lua
+++ .contrib/Parser/DATAS/00 - Database/Manual Item Filter Overrides.lua
@@ -30,4 +30,118 @@ i(15906, 34);	-- Left-Handed Brass Knuckles
 i(15907, 34);	-- Left-Handed Claw
 i(11505, 34);	-- Monster - Claw - Bear Offhand
 i(11506, 34);	-- Monster - Claw Offhand
-i(11863, 34);	-- White Bone Shredder
+i(11863, 34);	-- White Bone Shredder-- Mounts
+i(18243, 100);	-- Black Battlestrider
+i(21176, 100);	-- Black Qiraji Resonating Crystal
+i(13328, 100);	-- Black Ram
+i(2411, 100);	-- Black Stallion Bridle
+i(18247, 100);	-- Black War Kodo
+i(18244, 100);	-- Black War Ram
+i(18241, 100);	-- Black War Steed Bridle
+i(16343, 100);	-- Blood Guard's Mount [NYI]
+i(8595, 100);	-- Blue Mechanostrider
+i(21218, 100);	-- Blue Qiraji Resonating Crystal
+i(13332, 100);	-- Blue Skeletal Horse
+i(5656, 100);	-- Brown Horse Bridle
+i(875, 100);	-- Brown Horse Summoning [NYI]
+i(15290, 100);	-- Brown Kodo
+i(5872, 100);	-- Brown Ram
+i(13333, 100);	-- Brown Skeletal Horse
+i(5655, 100);	-- Chestnut Mare Bridle
+i(16339, 100);	-- Commander's Steed [NYI]
+i(13335, 100);	-- Deathcharger's Reins
+i(901, 100);	-- Deptecated White Stallion Summoning (Mount) [NYI]
+i(13325, 100);	-- Fluorescent Green Mechanostrider [NYI]
+i(20221, 100);	-- Foror's Fabled Steed [NYI]
+i(13329, 100);	-- Frost Ram
+i(15277, 100);	-- Gray Kodo
+i(5864, 100);	-- Gray Ram
+i(18794, 100);	-- Great Brown Kodo
+i(18795, 100);	-- Great Gray Kodo
+i(18793, 100);	-- Great White Kodo
+i(15292, 100);	-- Green Kodo
+i(13321, 100);	-- Green Mechanostrider
+i(21323, 100);	-- Green Qiraji Resonating Crystal
+i(13334, 100);	-- Green Skeletal Warhorse
+i(5874, 100);	-- Harness: Black Ram [NYI]
+i(5875, 100);	-- Harness: Blue Ram [NYI]
+i(12351, 100);	-- Horn of the Arctic Wolf
+i(18245, 100);	-- Horn of the Black War Wolf
+i(1041, 100);	-- Horn of the Black Wolf
+i(5668, 100);	-- Horn of the Brown Wolf
+i(5665, 100);	-- Horn of the Dire Wolf
+i(19029, 100);	-- Horn of the Frostwolf Howler
+i(1134, 100);	-- Horn of the Gray Wolf [NYI]
+i(12330, 100);	-- Horn of the Red Wolf [NYI]
+i(5663, 100);	-- Horn of the Red Wolf [NYI]
+i(8583, 100);	-- Horn of the Skeletal Mount [NYI]
+i(18796, 100);	-- Horn of the Swift Brown Wolf
+i(18798, 100);	-- Horn of the Swift Gray Wolf
+i(18797, 100);	-- Horn of the Swift Timber Wolf
+i(1132, 100);	-- Horn of the Timber Wolf
+i(1133, 100);	-- Horn of the Winter Wolf [NYI]
+i(13327, 100);	-- Icy Blue Mechanostrider Mod A [NYI]
+i(16338, 100);	-- Knight-Lieutenant's Steed [NYI]
+i(14062, 100);	-- Kodo Mount [NYI]
+i(16344, 100);	-- Lieutenant General's Mount [NYI]
+i(8589, 100);	-- Old Whistle of the Ivory Raptor [NYI]
+i(8590, 100);	-- Old Whistle of the Obsidian Raptor [NYI]
+i(2413, 100);	-- Palomino [NYI]
+i(12354, 100);	-- Palomino Bridle
+i(2414, 100);	-- Pinto Bridle
+i(13323, 100);	-- Purple Mechanostrider
+i(18791, 100);	-- Purple Skeletal Warhorse
+i(13324, 100);	-- Red & Blue Mechanostrider
+i(8563, 100);	-- Red Mechanostrider
+i(21321, 100);	-- Red Qiraji Resonating Crystal
+i(13331, 100);	-- Red Skeletal Horse
+i(18248, 100);	-- Red Skeletal Warhorse
+i(8630, 100);	-- Reins of the Bengal Tiger [NYI]
+i(18242, 100);	-- Reins of the Black War Tiger
+i(12302, 100);	-- Reins of the Frostsaber
+i(12327, 100);	-- Reins of the Golden Sabercat
+i(8633, 100);	-- Reins of the Leopard [NYI]
+i(8627, 100);	-- Reins of the Night saber [NYI]
+i(12303, 100);	-- Reins of the Nightsaber
+i(12325, 100);	-- Reins of the Primal Leopard [NYI]
+i(8632, 100);	-- Reins of the Spotted Frostsaber
+i(8628, 100);	-- Reins of the Spotted Nightsaber [NYI]
+i(8631, 100);	-- Reins of the Striped Frostsaber
+i(8629, 100);	-- Reins of the Striped Nightsaber
+i(18768, 100);	-- Reins of the Swift Dawnsaber [NYI]
+i(18766, 100);	-- Reins of the Swift Frostsaber
+i(18767, 100);	-- Reins of the Swift Mistsaber
+i(18902, 100);	-- Reins of the Swift Stormsaber
+i(12326, 100);	-- Reins of the Tawny Sabercat [NYI]
+i(13086, 100);	-- Reins of the Winterspring Frostsaber
+i(21736, 100);	-- Riding Gryphon Reins [NYI]
+i(23720, 100);	-- Riding Turtle
+i(23193, 100);	-- Skeletal Steed Reins [NYI]
+i(19030, 100);	-- Stormpike Battle Charger
+i(18788, 100);	-- Swift Blue Raptor
+i(18786, 100);	-- Swift Brown Ram
+i(18777, 100);	-- Swift Brown Steed
+i(18787, 100);	-- Swift Gray Ram
+i(18772, 100);	-- Swift Green Mechanostrider
+i(18789, 100);	-- Swift Olive Raptor
+i(18790, 100);	-- Swift Orange Raptor
+i(18776, 100);	-- Swift Palomino
+i(19872, 100);	-- Swift Razzashi Raptor
+i(18773, 100);	-- Swift White Mechanostrider
+i(18785, 100);	-- Swift White Ram
+i(18778, 100);	-- Swift White Steed
+i(18774, 100);	-- Swift Yellow Mechanostrider
+i(19902, 100);	-- Swift Zulian Tiger
+i(15293, 100);	-- Teal Kodo
+i(13322, 100);	-- Unpainted Mechanostrider
+i(18246, 100);	-- Whistle of the Black War Raptor
+i(8588, 100);	-- Whistle of the Emerald Raptor
+i(13317, 100);	-- Whistle of the Ivory Raptor
+i(8586, 100);	-- Whistle of the Mottled Red Raptor
+i(8591, 100);	-- Whistle of the Turquoise Raptor
+i(8592, 100);	-- Whistle of the Violet Raptor
+i(13326, 100);	-- White Mechanostrider Mod A
+i(5873, 100);	-- White Ram
+i(2415, 100);	-- White Stallion
+i(12353, 100);	-- White Stallion Bridle
+i(21324, 100);	-- Yellow Qiraji Resonating Crystal
