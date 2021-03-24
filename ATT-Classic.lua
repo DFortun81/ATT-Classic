fe986c35b1c5efcd45c22257a38a14359c8ec262
Crieve <dylanfortune@live.com>
2021-03-24 00:00:29 -0700

    Finished exploration for the Wetlands. And also finished the Exploration Module!

--- ATT-Classic.lua
+++ ATT-Classic.lua
@@ -2349,8 +2349,7 @@ local function AddTomTomWaypoint(group, auto)
 				world = true
 			};
 			if group.title then opt.title = opt.title .. "\n" .. group.title; end
-			if group.subAreaID then opt.title = opt.title .. "\nSub Area ID " .. group.subAreaID; end
-			if group.explorationID then opt.title = opt.title .. "\nExploration ID " .. group.explorationID; end
+			--if group.explorationID then opt.title = opt.title .. "\nExploration ID " .. group.explorationID; end
 			local defaultMapID = GetRelativeMap(group, app.GetCurrentMapID());
 			local displayID = GetDisplayID(group);
 			if displayID then
@@ -5060,42 +5059,27 @@ local EXPLORATION_ID_MAP = setmetatable({
 		]]--
 	},
 	[1243] = {	-- Wetlands
-		["175:128:13:314"] = 1,
-		["185:240:456:125"] = 2,
-		["190:160:628:176"] = 3,
-		["195:185:247:205"] = 4,
-		["200:185:349:115"] = 5,
-		["200:240:237:41"] = 6,
-		["205:180:401:21"] = 7,
-		["205:245:527:264"] = 8,
-		["225:185:347:218"] = 9,
-		["225:190:89:142"] = 10,
-		["230:190:470:371"] = 11,
-		["240:175:77:245"] = 12,
-		["256:250:507:115"] = 13,
-		["300:240:92:82"] = 14,
-		["350:360:611:230"] = 15,
+		["175:128:13:314"] = 150,	-- Menethil Harbor
+		["185:240:456:125"] = 1025,	-- The Green Belt
+		["190:160:628:176"] = 1017,	-- Raptor Ridge
+		["195:185:247:205"] = 118,	-- Whelgar's Excavation Site
+		["200:185:349:115"] = 309,	-- Ironbeard's Tomb
+		["200:240:237:41"] = 1023,	-- Saltspray Glen
+		["205:180:401:21"] = 205,	-- Dun Modr
+		["205:245:527:264"] = 1020,	-- Mosshide Fen
+		["225:185:347:218"] = 1036,	-- Angerfang Encampment
+		["225:190:89:142"] = 1022,	-- Bluegill Marsh
+		["230:190:470:371"] = 1021,	-- Thelgen Rock
+		["240:175:77:245"] = 1018,	-- Black Channel Marsh
+		["256:250:507:115"] = 1016,	-- Direforge Hill
+		["300:240:92:82"] = 1024,	-- Sundown Marsh
+		["350:360:611:230"] = 1038,	-- Dragonmaw Gates
 		--[[
-		[118] = 1,                               -- Whelgar's Excavation Site
-		[150] = 2,                               -- Menethil Harbor
-		[205] = 3,                               -- Dun Modr
 		[298] = 4,                               -- Baradin Bay
 		[299] = 5,                               -- Menethil Bay
-		[309] = 6,                               -- Ironbeard's Tomb
 		[836] = 7,                               -- Dun Algaz
 		[881] = 8,                               -- Thandol Span
-		[1016] = 9,                              -- Direforge Hill
-		[1017] = 10,                             -- Raptor Ridge
-		[1018] = 11,                             -- Black Channel Marsh
-		[1020] = 12,                             -- Mosshide Fen
-		[1021] = 13,                             -- Thelgen Rock
-		[1022] = 14,                             -- Bluegill Marsh
-		[1023] = 15,                             -- Saltspray Glen
-		[1024] = 16,                             -- Sundown Marsh
-		[1025] = 17,                             -- The Green Belt
-		[1036] = 18,                             -- Angerfang Encampment
 		[1037] = 19,                             -- Grim Batol
-		[1038] = 20,                             -- Dragonmaw Gates
 		[1039] = 21,                             -- The Lost Fleet
 		[2103] = 22,                             -- Menethil Keep
 		[2104] = 23,                             -- Deepwater Tavern
@@ -5350,7 +5334,7 @@ app.ExplorationClass = {
 		if key == "key" then
 			return "explorationID";
 		elseif key == "text" then
-			return (t.explorationID < 40 and t.hash) or C_Map.GetAreaInfo(t.explorationID) or t.hash or t.explorationID;
+			return C_Map.GetAreaInfo(t.explorationID) or t.hash;
 		elseif key == "icon" then
 			return "Interface\\Addons\\ATT-Classic\\assets\\INV_Misc_Map02";
 		elseif key == "preview" then
