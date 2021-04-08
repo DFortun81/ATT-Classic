51d4049930cd92d463e1c21a7daa4a58ec9481a0
Crieve <dylanfortune@live.com>
2021-04-08 04:39:55 -0700

    Updated koKR locale.

--- locales/koKR.lua
+++ locales/koKR.lua
@@ -7,59 +7,78 @@ local L = app.L;
 
 --TODO: L.TOGGLE_FACTION_MODE = "Toggle Faction Mode";
 
--- Dungeons
-L.ZONE_TEXT_TO_MAP_ID["검은심연 나락"] = 221;	-- BFD
-L.ZONE_TEXT_TO_MAP_ID["검은바위 나락"] = 242;	-- BRD
-L.ZONE_TEXT_TO_MAP_ID["검은바위 산"] = 35;	-- BRM
-L.ZONE_TEXT_TO_MAP_ID["검은바위 첨탑"] = 250;	-- BRS
-L.ZONE_TEXT_TO_MAP_ID["죽음의 폐광"] = 291;	-- DM/VC
-L.ZONE_TEXT_TO_MAP_ID["혈투의 전장"] = 234;	-- Dire Maul
-L.ZONE_TEXT_TO_MAP_ID["놈리건"] = 226;	-- Gnomer
-L.ZONE_TEXT_TO_MAP_ID["마라우돈"] = 280;	-- Maraudon
-L.ZONE_TEXT_TO_MAP_ID["성난불길 협곡"] = 213;	-- RFC
-L.ZONE_TEXT_TO_MAP_ID["가시덩굴 구릉"] = 300;	-- RFD
-L.ZONE_TEXT_TO_MAP_ID["가시덩굴 우리"] = 301;	-- RFK
-L.ZONE_TEXT_TO_MAP_ID["스칼로맨스"] = 310;	-- SCHOLO
-L.ZONE_TEXT_TO_MAP_ID["그림자송곳니 성채"] = 310;	-- SFK
-L.ZONE_TEXT_TO_MAP_ID["붉은십자군 수도원"] = 435;	-- SM
-L.ZONE_TEXT_TO_MAP_ID["스트라솔름"] = 317;	-- STRATH
-L.ZONE_TEXT_TO_MAP_ID["아탈학카르 신전"] = 220;	-- ST
-L.ZONE_TEXT_TO_MAP_ID["울다만"] = 230;	-- ULDA
-L.ZONE_TEXT_TO_MAP_ID["통곡의 동굴"] = 279;	-- WC
-L.ZONE_TEXT_TO_MAP_ID["줄파락"] = 219;	-- ZF
+local a = L.ZONE_TEXT_TO_MAP_ID;
+for key,value in pairs({
+	-- Dungeons
+	["검은심연 나락"] = 221,	-- BFD
+	["검은바위 나락"] = 242,	-- BRD
+	["검은바위 산"] = 35,	-- BRM
+	["검은바위 첨탑"] = 250,	-- BRS
+	["죽음의 폐광"] = 291,	-- DM/VC
+	["혈투의 전장"] = 234,	-- Dire Maul
+	["놈리건"] = 226,	-- Gnomer
+	["마라우돈"] = 280,	-- Maraudon
+	["성난불길 협곡"] = 213,	-- RFC
+	["가시덩굴 구릉"] = 300,	-- RFD
+	["가시덩굴 우리"] = 301,	-- RFK
+	["스칼로맨스"] = 310,	-- SCHOLO
+	["그림자송곳니 성채"] = 310,	-- SFK
+	["붉은십자군 수도원"] = 435,	-- SM
+	["스트라솔름"] = 317,	-- STRATH
+	["아탈학카르 신전"] = 220,	-- ST
+	["울다만"] = 230,	-- ULDA
+	["통곡의 동굴"] = 279,	-- WC
+	["줄파락"] = 219,	-- ZF
+	
+	-- TBC
+	["카라잔"] = 350,		-- Karazhan
+	["그룰의 둥지"] = 330,	-- Gruul's Lair
+	["마그테리돈의 둥지"] = 331,	-- Magtheridon's Lair
+	["불뱀 제단"] = 332,	-- Serpentshrine Cavern
+	["폭풍우 요새"] = 334,	-- Tempest Keep
+	["하이잘 정상"] = 329,	-- The Battle for Mount Hyjal
+	["검은 사원"] = 340,	-- The Black Temple
+	["태양샘 고원"] = 335,	-- Sunwell Plataeu
+	["줄아만"] = 333,		-- Zul'Aman
+	["마법학자의 정원"] = 348,	-- Magister's Terrace
+})
+do a[key] = value; end
 
-L.ZONE_TEXT_TO_MAP_ID["Magister's Terrace"] = 348;	-- MGT
-L.ZONE_TEXT_TO_MAP_ID["Karazhan"] = 350;	-- KARA
-L.ZONE_TEXT_TO_MAP_ID["Gruul's Lair"] = 330;	-- GRUL
-L.ZONE_TEXT_TO_MAP_ID["Magtheridon's Lair"] = 331;	-- MAG
-L.ZONE_TEXT_TO_MAP_ID["Serpentshrine Cavern"] = 332;	-- SSC
-L.ZONE_TEXT_TO_MAP_ID["폭풍우 요새"] = 334;	-- TK
-L.ZONE_TEXT_TO_MAP_ID["The Battle for Mount Hyjal"] = 329;	-- HYJAL
-L.ZONE_TEXT_TO_MAP_ID["The Black Temple"] = 340;	-- BT
-L.ZONE_TEXT_TO_MAP_ID["Sunwell Plataeu"] = 335;	-- SWP
-L.ZONE_TEXT_TO_MAP_ID["Zul'Aman"] = 333;	-- ZA
+local a = L.ALT_ZONE_TEXT_TO_MAP_ID;
+for key,value in pairs({
+	-- Classic
+	["안퀴라즈 사원"] = 320,	-- Temple of Ahn'Qiraj
+	["안퀴라즈 관문"] = 1451,	-- Gates of Ahn'Qiraj
 
-L.ALT_ZONE_TEXT_TO_MAP_ID["아키나이 납골당"] = 256;	-- CRYPTS
-L.ALT_ZONE_TEXT_TO_MAP_ID["마나 무덤"] = 272;	-- MT
-L.ALT_ZONE_TEXT_TO_MAP_ID["세데크 전당"] = 258;	-- SETH
-L.ALT_ZONE_TEXT_TO_MAP_ID["어둠의 미궁"] = 260;	-- SLABS
-L.ALT_ZONE_TEXT_TO_MAP_ID["검은늪"] = 273;	-- BLKM
-L.ALT_ZONE_TEXT_TO_MAP_ID["Old Hillsbrad Foothills"] = 274;	-- OHF
-L.ALT_ZONE_TEXT_TO_MAP_ID["The Slave Pens"] = 265;	-- SPENS
-L.ALT_ZONE_TEXT_TO_MAP_ID["The Steamvault"] = 263;	-- SVALT
-L.ALT_ZONE_TEXT_TO_MAP_ID["The Underbog"] = 262;	-- UNBOG
-L.ALT_ZONE_TEXT_TO_MAP_ID["The Blood Furnace"] = 261;	-- BF
-L.ALT_ZONE_TEXT_TO_MAP_ID["Hellfire Ramparts"] = 347;	-- RAMPS
-L.ALT_ZONE_TEXT_TO_MAP_ID["The Shattered Halls"] = 246;	-- SHALLS
-L.ALT_ZONE_TEXT_TO_MAP_ID["The Arcatraz"] = 269;	-- ARCA
-L.ALT_ZONE_TEXT_TO_MAP_ID["The Botanica"] = 266;	-- BOT
-L.ALT_ZONE_TEXT_TO_MAP_ID["The Mechanar"] = 267;	-- MECH
+	-- TBC
+	["폭풍우 요새"] = 334,	-- The Eye
+	["아키나이 납골당"] = 256,	-- Auchenai Crypts
+	["마나 무덤"] = 272,	-- Mana Tombs
+	["세데크 전당"] = 258,	-- Sethekk Halls
+	["어둠의 미궁"] = 260,	-- Shadow Labyrinth
+	["검은늪"] = 273,		-- The Black Morass
+	["옛 언덕마루 구릉지"] = 274,	-- Old Hillsbrad Foothills
+	["강제 노역소"] = 265,	-- The Slave Pens
+	["증기 저장고"] = 263,	-- The Steamvault
+	["지하 수렁"] = 262,	-- The Underbog
+	["피의 용광로"] = 261,	-- The Blood Furnace
+	["지옥불 성루"] = 347,	-- Hellfire Ramparts
+	["으스러진 손의 전당"] = 246,	-- The Shattered Halls
+	["알카트라즈"] = 269,	-- The Arcatraz
+	["신록의 정원"] = 266,	-- The Botanica
+	["메카나르"] = 267,		-- The Mechanar
+})
+do a[key] = value; end
 
-L.ALT_PROFESSION_TEXT_TO_ID["가죽 세공"] = 2108;	-- Leatherworking
+local a = L.ALT_PROFESSION_TEXT_TO_ID;
+for key,value in pairs({
+	["가죽 세공"] = 2108,	-- Leatherworking
+})
+do a[key] = value; end
 
 local a = L.HEADER_NAMES;
 for key,value in pairs({
-	-- Enter translated NPCID's here
+	-- Enter translated headers here
 })
 do a[key] = value; end
 
