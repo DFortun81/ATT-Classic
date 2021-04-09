5d6b615b1f8c7b9072c61dd022c4fa5a2ceaf4ac
Crieve <dylanfortune@live.com>
2021-04-09 02:31:57 -0700

    Moved PvP Honor Titles to their own section.

---
+++ .contrib/Parser/DATAS/09 - PvP/01. Honor Titles/Honor Titles.lua
@@ -0,0 +1,25 @@
+_.PvP = 
+{
+	n(-32, {	-- Honor Titles
+		["timeline"] = {
+			"added 1.11.1.6692",
+			"removed 2.0.0.13287"
+		},
+		["groups"] = {
+			pvprank(1),		-- Private / Scout
+			pvprank(2),		-- Corporal / Grunt
+			pvprank(3),		-- Sergeant / Sergeant
+			pvprank(4),		-- Master Sergeant / Senior Sergeant
+			pvprank(5),		-- Sergeant Major / First Sergeant
+			pvprank(6),		-- Knight / Stone Guard
+			pvprank(7),		-- Knight-Lieutenant / Blood Guard
+			pvprank(8),		-- Knight-Captain / Legionnaire
+			pvprank(9),		-- Knight-Champion / Centurion
+			pvprank(10),	-- Lieutenant Commander / Champion
+			pvprank(11),	-- Commander / Lieutenant General
+			pvprank(12),	-- Marshal / General
+			pvprank(13),	-- Field Marshal / Warlord
+			pvprank(14),	-- Grand Marshal / High Warlord
+		},
+	}),
+};
--- .contrib/Parser/DATAS/09 - PvP/02. Battlegrounds/Alterac Valley.lua
+++ .contrib/Parser/DATAS/09 - PvP/02. Battlegrounds/Alterac Valley.lua
@@ -95,176 +95,6 @@ _.PvP =
 					["icon"] = "Interface\\Icons\\INV_Jewelry_StormPikeTrinket_05",
 					["races"] = ALLIANCE_ONLY,
 				}),
-				n(VENDORS, {
-					n(13216, {	-- Gaelden Hammersmith <Stormpike Supply Officer>
-						["coord"] = { 44.3, 18.3, ALTERAC_VALLEY },
-						["races"] = ALLIANCE_ONLY,
-						["groups"] = {
-							i(19100),	-- Electrified Dagger
-							i(19102),	-- Crackling Staff
-							i(19104),	-- Stormstrike Hammer
-							i(19308),	-- Tome of Arcane Domination
-							i(19309),	-- Tome of Shadow Force
-							i(19310),	-- Tome of the Ice Lord
-							i(19311),	-- Tome of Fiery Arcana
-							i(19312),	-- Lei of the Lifegiver
-							i(19315),	-- Therazane's Touch
-							i(19321),	-- The Immovable Object
-							i(19323),	-- The Unstoppable Force
-							i(19324),	-- The Lobotomizer
-							i(19325),	-- Don Julio's Band
-							un(PHASE_FIVE, i(21563)),	-- Don Rodrigo's Band
-							i(19030),	-- Stormpike Battle Charger
-							i(19084),	-- Stormpike Soldier's Cloak
-							i(19086),	-- Stormpike Sage's Cloak
-							i(19091),	-- Stormpike Plate Girdle
-							i(19092),	-- Stormpike Mail Girdle
-							i(19093),	-- Stormpike Leather Girdle
-							i(19094),	-- Stormpike Cloth Girdle
-							i(19097),	-- Stormpike Soldier's Pendant
-							i(19098),	-- Stormpike Sage's Pendant
-							i(19301),	-- Alterac Manna Biscuit
-							i(19319),	-- Harpy Hide Quiver
-							i(19320),	-- Gnoll Skin Bandolier
-							i(19307),	-- Alterac Heavy Runecloth Bandage
-							i(17348),	-- Major Healing Draught
-							i(17351),	-- Major Mana Draught
-							i(19318),	-- Bottled Alterac Spring Water
-							i(19316),	-- Ice Threaded Arrow
-							i(19317),	-- Ice Threaded Bullet
-							i(17349),	-- Superior Healing Draught
-							i(17352),	-- Superior Mana Draught
-							i(19032),	-- Stormpike Battle Tabard
-							i(19045),	-- Stormpike Battle Standard
-						},
-					}),
-					n(13218, {	-- Grunnda Wolfheart <Frostwolf Supply Officer>
-						["coord"] = { 49.3, 82.5, ALTERAC_VALLEY },
-						["races"] = HORDE_ONLY,
-						["groups"] = {
-							i(19099),	-- Glacial Blade
-							i(19101),	-- Whiteout Staff
-							i(19103),	-- Frostbite
-							i(19308),	-- Tome of Arcane Domination
-							i(19309),	-- Tome of Shadow Force
-							i(19310),	-- Tome of the Ice Lord
-							i(19311),	-- Tome of Fiery Arcana
-							i(19312),	-- Lei of the Lifegiver
-							i(19315),	-- Therazane's Touch
-							i(19321),	-- The Immovable Object
-							i(19323),	-- The Unstoppable Force
-							i(19324),	-- The Lobotomizer
-							i(19325),	-- Don Julio's Band
-							un(PHASE_FIVE, i(21563)),	-- Don Rodrigo's Band
-							i(19029),	-- Horn of the Frostwolf Howler
-							i(19083),	-- Frostwolf Legionnaire's Cloak
-							i(19085),	-- Frostwolf Advisor's Cloak
-							i(19087),	-- Frostwolf Plate Belt
-							i(19088),	-- Frostwolf Mail Belt
-							i(19089),	-- Frostwolf Leather Belt
-							i(19090),	-- Frostwolf Cloth Belt
-							i(19095),	-- Frostwolf Legionnaire's Pendant
-							i(19096),	-- Frostwolf Advisor's Pendant
-							i(19301),	-- Alterac Manna Biscuit
-							i(19319),	-- Harpy Hide Quiver
-							i(19320),	-- Gnoll Skin Bandolier
-							i(19307),	-- Alterac Heavy Runecloth Bandage
-							i(17348),	-- Major Healing Draught
-							i(17351),	-- Major Mana Draught
-							i(19318),	-- Bottled Alterac Spring Water
-							i(19316),	-- Ice Threaded Arrow
-							i(19317),	-- Ice Threaded Bullet
-							i(17349),	-- Superior Healing Draught
-							i(17352),	-- Superior Mana Draught
-							i(19031),	-- Frostwolf Battle Tabard
-							i(19046),	-- Frostwolf Battle Standard
-						},
-					}),
-					n(13219, {	-- Jekyll Flandring <Frostwolf Supply Officer>
-						["coord"] = { 62.8, 59.4, ALTERAC_MOUNTAINS },
-						["races"] = HORDE_ONLY,
-						["groups"] = {
-							i(19099),	-- Glacial Blade
-							i(19101),	-- Whiteout Staff
-							i(19103),	-- Frostbite
-							i(19308),	-- Tome of Arcane Domination
-							i(19309),	-- Tome of Shadow Force
-							i(19310),	-- Tome of the Ice Lord
-							i(19311),	-- Tome of Fiery Arcana
-							i(19312),	-- Lei of the Lifegiver
-							i(19315),	-- Therazane's Touch
-							i(19321),	-- The Immovable Object
-							i(19323),	-- The Unstoppable Force
-							i(19324),	-- The Lobotomizer
-							i(19325),	-- Don Julio's Band
-							un(PHASE_FIVE, i(21563)),	-- Don Rodrigo's Band
-							i(19029),	-- Horn of the Frostwolf Howler
-							i(19083),	-- Frostwolf Legionnaire's Cloak
-							i(19085),	-- Frostwolf Advisor's Cloak
-							i(19087),	-- Frostwolf Plate Belt
-							i(19088),	-- Frostwolf Mail Belt
-							i(19089),	-- Frostwolf Leather Belt
-							i(19090),	-- Frostwolf Cloth Belt
-							i(19095),	-- Frostwolf Legionnaire's Pendant
-							i(19096),	-- Frostwolf Advisor's Pendant
-							i(19301),	-- Alterac Manna Biscuit
-							i(19319),	-- Harpy Hide Quiver
-							i(19320),	-- Gnoll Skin Bandolier
-							i(19307),	-- Alterac Heavy Runecloth Bandage
-							i(17348),	-- Major Healing Draught
-							i(17351),	-- Major Mana Draught
-							i(19318),	-- Bottled Alterac Spring Water
-							i(19316),	-- Ice Threaded Arrow
-							i(19317),	-- Ice Threaded Bullet
-							i(17349),	-- Superior Healing Draught
-							i(17352),	-- Superior Mana Draught
-							i(19031),	-- Frostwolf Battle Tabard
-							i(19046),	-- Frostwolf Battle Standard
-						},
-					}),
-					n(13217, {	-- Thanthaldis Snowgleam <Stormpike Supply Officer>
-						["coord"] = { 39.6, 81.6, ALTERAC_MOUNTAINS },
-						["races"] = ALLIANCE_ONLY,
-						["groups"] = {
-							i(19100),	-- Electrified Dagger
-							i(19102),	-- Crackling Staff
-							i(19104),	-- Stormstrike Hammer
-							i(19308),	-- Tome of Arcane Domination
-							i(19309),	-- Tome of Shadow Force
-							i(19310),	-- Tome of the Ice Lord
-							i(19311),	-- Tome of Fiery Arcana
-							i(19312),	-- Lei of the Lifegiver
-							i(19315),	-- Therazane's Touch
-							i(19321),	-- The Immovable Object
-							i(19323),	-- The Unstoppable Force
-							i(19324),	-- The Lobotomizer
-							i(19325),	-- Don Julio's Band
-							un(PHASE_FIVE, i(21563)),	-- Don Rodrigo's Band
-							i(19030),	-- Stormpike Battle Charger
-							i(19084),	-- Stormpike Soldier's Cloak
-							i(19086),	-- Stormpike Sage's Cloak
-							i(19091),	-- Stormpike Plate Girdle
-							i(19092),	-- Stormpike Mail Girdle
-							i(19093),	-- Stormpike Leather Girdle
-							i(19094),	-- Stormpike Cloth Girdle
-							i(19097),	-- Stormpike Soldier's Pendant
-							i(19098),	-- Stormpike Sage's Pendant
-							i(19301),	-- Alterac Manna Biscuit
-							i(19319),	-- Harpy Hide Quiver
-							i(19320),	-- Gnoll Skin Bandolier
-							i(19307),	-- Alterac Heavy Runecloth Bandage
-							i(17348),	-- Major Healing Draught
-							i(17351),	-- Major Mana Draught
-							i(19318),	-- Bottled Alterac Spring Water
-							i(19316),	-- Ice Threaded Arrow
-							i(19317),	-- Ice Threaded Bullet
-							i(17349),	-- Superior Healing Draught
-							i(17352),	-- Superior Mana Draught
-							i(19032),	-- Stormpike Battle Tabard
-							i(19045),	-- Stormpike Battle Standard
-						},
-					}),
-				}),
 				n(QUESTS, {
 					q(7385, {	-- A Gallon of Blood
 						["qg"] = 13236,	-- Primalist Thurloga
@@ -820,6 +650,176 @@ _.PvP =
 						["coord"] = { 65.4, 55.2, ALTERAC_MOUNTAINS },
 						["races"] = HORDE_ONLY,
 						["lvl"] = 51,
+					}),
+				}),
+				n(VENDORS, {
+					n(13216, {	-- Gaelden Hammersmith <Stormpike Supply Officer>
+						["coord"] = { 44.3, 18.3, ALTERAC_VALLEY },
+						["races"] = ALLIANCE_ONLY,
+						["groups"] = {
+							i(19100),	-- Electrified Dagger
+							i(19102),	-- Crackling Staff
+							i(19104),	-- Stormstrike Hammer
+							i(19308),	-- Tome of Arcane Domination
+							i(19309),	-- Tome of Shadow Force
+							i(19310),	-- Tome of the Ice Lord
+							i(19311),	-- Tome of Fiery Arcana
+							i(19312),	-- Lei of the Lifegiver
+							i(19315),	-- Therazane's Touch
+							i(19321),	-- The Immovable Object
+							i(19323),	-- The Unstoppable Force
+							i(19324),	-- The Lobotomizer
+							i(19325),	-- Don Julio's Band
+							un(PHASE_FIVE, i(21563)),	-- Don Rodrigo's Band
+							i(19030),	-- Stormpike Battle Charger
+							i(19084),	-- Stormpike Soldier's Cloak
+							i(19086),	-- Stormpike Sage's Cloak
+							i(19091),	-- Stormpike Plate Girdle
+							i(19092),	-- Stormpike Mail Girdle
+							i(19093),	-- Stormpike Leather Girdle
+							i(19094),	-- Stormpike Cloth Girdle
+							i(19097),	-- Stormpike Soldier's Pendant
+							i(19098),	-- Stormpike Sage's Pendant
+							i(19301),	-- Alterac Manna Biscuit
+							i(19319),	-- Harpy Hide Quiver
+							i(19320),	-- Gnoll Skin Bandolier
+							i(19307),	-- Alterac Heavy Runecloth Bandage
+							i(17348),	-- Major Healing Draught
+							i(17351),	-- Major Mana Draught
+							i(19318),	-- Bottled Alterac Spring Water
+							i(19316),	-- Ice Threaded Arrow
+							i(19317),	-- Ice Threaded Bullet
+							i(17349),	-- Superior Healing Draught
+							i(17352),	-- Superior Mana Draught
+							i(19032),	-- Stormpike Battle Tabard
+							i(19045),	-- Stormpike Battle Standard
+						},
+					}),
+					n(13218, {	-- Grunnda Wolfheart <Frostwolf Supply Officer>
+						["coord"] = { 49.3, 82.5, ALTERAC_VALLEY },
+						["races"] = HORDE_ONLY,
+						["groups"] = {
+							i(19099),	-- Glacial Blade
+							i(19101),	-- Whiteout Staff
+							i(19103),	-- Frostbite
+							i(19308),	-- Tome of Arcane Domination
+							i(19309),	-- Tome of Shadow Force
+							i(19310),	-- Tome of the Ice Lord
+							i(19311),	-- Tome of Fiery Arcana
+							i(19312),	-- Lei of the Lifegiver
+							i(19315),	-- Therazane's Touch
+							i(19321),	-- The Immovable Object
+							i(19323),	-- The Unstoppable Force
+							i(19324),	-- The Lobotomizer
+							i(19325),	-- Don Julio's Band
+							un(PHASE_FIVE, i(21563)),	-- Don Rodrigo's Band
+							i(19029),	-- Horn of the Frostwolf Howler
+							i(19083),	-- Frostwolf Legionnaire's Cloak
+							i(19085),	-- Frostwolf Advisor's Cloak
+							i(19087),	-- Frostwolf Plate Belt
+							i(19088),	-- Frostwolf Mail Belt
+							i(19089),	-- Frostwolf Leather Belt
+							i(19090),	-- Frostwolf Cloth Belt
+							i(19095),	-- Frostwolf Legionnaire's Pendant
+							i(19096),	-- Frostwolf Advisor's Pendant
+							i(19301),	-- Alterac Manna Biscuit
+							i(19319),	-- Harpy Hide Quiver
+							i(19320),	-- Gnoll Skin Bandolier
+							i(19307),	-- Alterac Heavy Runecloth Bandage
+							i(17348),	-- Major Healing Draught
+							i(17351),	-- Major Mana Draught
+							i(19318),	-- Bottled Alterac Spring Water
+							i(19316),	-- Ice Threaded Arrow
+							i(19317),	-- Ice Threaded Bullet
+							i(17349),	-- Superior Healing Draught
+							i(17352),	-- Superior Mana Draught
+							i(19031),	-- Frostwolf Battle Tabard
+							i(19046),	-- Frostwolf Battle Standard
+						},
+					}),
+					n(13219, {	-- Jekyll Flandring <Frostwolf Supply Officer>
+						["coord"] = { 62.8, 59.4, ALTERAC_MOUNTAINS },
+						["races"] = HORDE_ONLY,
+						["groups"] = {
+							i(19099),	-- Glacial Blade
+							i(19101),	-- Whiteout Staff
+							i(19103),	-- Frostbite
+							i(19308),	-- Tome of Arcane Domination
+							i(19309),	-- Tome of Shadow Force
+							i(19310),	-- Tome of the Ice Lord
+							i(19311),	-- Tome of Fiery Arcana
+							i(19312),	-- Lei of the Lifegiver
+							i(19315),	-- Therazane's Touch
+							i(19321),	-- The Immovable Object
+							i(19323),	-- The Unstoppable Force
+							i(19324),	-- The Lobotomizer
+							i(19325),	-- Don Julio's Band
+							un(PHASE_FIVE, i(21563)),	-- Don Rodrigo's Band
+							i(19029),	-- Horn of the Frostwolf Howler
+							i(19083),	-- Frostwolf Legionnaire's Cloak
+							i(19085),	-- Frostwolf Advisor's Cloak
+							i(19087),	-- Frostwolf Plate Belt
+							i(19088),	-- Frostwolf Mail Belt
+							i(19089),	-- Frostwolf Leather Belt
+							i(19090),	-- Frostwolf Cloth Belt
+							i(19095),	-- Frostwolf Legionnaire's Pendant
+							i(19096),	-- Frostwolf Advisor's Pendant
+							i(19301),	-- Alterac Manna Biscuit
+							i(19319),	-- Harpy Hide Quiver
+							i(19320),	-- Gnoll Skin Bandolier
+							i(19307),	-- Alterac Heavy Runecloth Bandage
+							i(17348),	-- Major Healing Draught
+							i(17351),	-- Major Mana Draught
+							i(19318),	-- Bottled Alterac Spring Water
+							i(19316),	-- Ice Threaded Arrow
+							i(19317),	-- Ice Threaded Bullet
+							i(17349),	-- Superior Healing Draught
+							i(17352),	-- Superior Mana Draught
+							i(19031),	-- Frostwolf Battle Tabard
+							i(19046),	-- Frostwolf Battle Standard
+						},
+					}),
+					n(13217, {	-- Thanthaldis Snowgleam <Stormpike Supply Officer>
+						["coord"] = { 39.6, 81.6, ALTERAC_MOUNTAINS },
+						["races"] = ALLIANCE_ONLY,
+						["groups"] = {
+							i(19100),	-- Electrified Dagger
+							i(19102),	-- Crackling Staff
+							i(19104),	-- Stormstrike Hammer
+							i(19308),	-- Tome of Arcane Domination
+							i(19309),	-- Tome of Shadow Force
+							i(19310),	-- Tome of the Ice Lord
+							i(19311),	-- Tome of Fiery Arcana
+							i(19312),	-- Lei of the Lifegiver
+							i(19315),	-- Therazane's Touch
+							i(19321),	-- The Immovable Object
+							i(19323),	-- The Unstoppable Force
+							i(19324),	-- The Lobotomizer
+							i(19325),	-- Don Julio's Band
+							un(PHASE_FIVE, i(21563)),	-- Don Rodrigo's Band
+							i(19030),	-- Stormpike Battle Charger
+							i(19084),	-- Stormpike Soldier's Cloak
+							i(19086),	-- Stormpike Sage's Cloak
+							i(19091),	-- Stormpike Plate Girdle
+							i(19092),	-- Stormpike Mail Girdle
+							i(19093),	-- Stormpike Leather Girdle
+							i(19094),	-- Stormpike Cloth Girdle
+							i(19097),	-- Stormpike Soldier's Pendant
+							i(19098),	-- Stormpike Sage's Pendant
+							i(19301),	-- Alterac Manna Biscuit
+							i(19319),	-- Harpy Hide Quiver
+							i(19320),	-- Gnoll Skin Bandolier
+							i(19307),	-- Alterac Heavy Runecloth Bandage
+							i(17348),	-- Major Healing Draught
+							i(17351),	-- Major Mana Draught
+							i(19318),	-- Bottled Alterac Spring Water
+							i(19316),	-- Ice Threaded Arrow
+							i(19317),	-- Ice Threaded Bullet
+							i(17349),	-- Superior Healing Draught
+							i(17352),	-- Superior Mana Draught
+							i(19032),	-- Stormpike Battle Tabard
+							i(19045),	-- Stormpike Battle Standard
+						},
 					}),
 				}),
 				n(ZONEDROPS, {
