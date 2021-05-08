8185ad81237803a54f1656efcce8e94519752454
Crieve <dylanfortune@live.com>
2021-05-07 23:59:17 -0700

    Added race filtering for Hellfire Peninsula flight masters.

--- .contrib/.db/02 - Outdoor Zones/03 Outland/Hellfire Peninsula/Flight Paths.lua
+++ .contrib/.db/02 - Outdoor Zones/03 Outland/Hellfire Peninsula/Flight Paths.lua
@@ -9,24 +9,31 @@ _.Zones =
 			n(FLIGHT_PATHS, {
 				fp(102, {	-- Falcon Watch, Hellfire Peninsula
 					["coord"] = { 27.8, 60.0, HELLFIRE_PENINSULA },
+					["races"] = HORDE_ONLY,
 				}),
-				fp(129, {	-- Hellfire Peninsula, The Dark Portal
+				fp(129, {	-- Hellfire Peninsula, The Dark Portal, Alliance
 					["coord"] = { 87.4, 52.4, HELLFIRE_PENINSULA },
+					["races"] = ALLIANCE_ONLY,
 				}),
-				fp(130, {	-- Hellfire Peninsula, The Dark Portal
+				fp(130, {	-- Hellfire Peninsula, The Dark Portal, Horde
 					["coord"] = { 87.4, 48.2, HELLFIRE_PENINSULA },
+					["races"] = HORDE_ONLY,
 				}),
 				fp(100, {	-- Honor Hold, Hellfire Peninsula
 					["coord"] = { 54.6, 62.4, HELLFIRE_PENINSULA },
+					["races"] = ALLIANCE_ONLY,
 				}),
 				fp(141, {	-- Spinebreaker Ridge, Hellfire Peninsula
 					["coord"] = { 61.6, 81.2, HELLFIRE_PENINSULA },
+					["races"] = HORDE_ONLY,
 				}),
 				fp(101, {	-- Temple of Telhamat, Hellfire Peninsula
 					["coord"] = { 25.2, 37.2, HELLFIRE_PENINSULA },
+					["races"] = ALLIANCE_ONLY,
 				}),
 				fp(99, {	-- Thrallmar, Hellfire Peninsula
 					["coord"] = { 56.2, 36.2, HELLFIRE_PENINSULA },
+					["races"] = HORDE_ONLY,
 				}),
 			}),
 		}),
--- db/Categories.lua
+++ db/Categories.lua
@@ -2,7 +2,7 @@
 --   S O U R C E   D A T A B A S E   M O D U L E   --
 -----------------------------------------------------
 --   WARNING: This file is dynamically generated   --
---   UPDATED: Saturday, May 8, 2021 @ 6:54 AM      --
+--   UPDATED: Saturday, May 8, 2021 @ 6:57 AM      --
 -----------------------------------------------------
 local _ = select(2, ...); local rawset = rawset;
 local g = function(t,g) rawset(t,'g',g); return t; end
