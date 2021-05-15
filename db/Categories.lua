a4462d2b7b30c181a89b802c1a59c6710e620163
Crieve <dylanfortune@live.com>
2021-05-15 12:10:38 -0700

    Explicitly marked the spellIDs associated with the recipes in Black Temple so that they can be collected.

--- db/Categories.lua
+++ db/Categories.lua
@@ -2,7 +2,7 @@
 --   S O U R C E   D A T A B A S E   M O D U L E   --
 -----------------------------------------------------
 --   WARNING: This file is dynamically generated   --
---   UPDATED: Saturday, May 15, 2021 @ 10:34 AM    --
+--   UPDATED: Saturday, May 15, 2021 @ 7:10 PM     --
 -----------------------------------------------------
 local _ = select(2, ...); local rawset = rawset;
 local g = function(t,g) rawset(t,'g',g); return t; end
