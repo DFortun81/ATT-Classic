89b0d6f4324acab9d666d3a12fdde33ecbc82efc
Crieve <dylanfortune@live.com>
2021-05-16 16:00:52 -0700

    Added the Ghostlands.

--- db/Categories.lua
+++ db/Categories.lua
@@ -2,7 +2,7 @@
 --   S O U R C E   D A T A B A S E   M O D U L E   --
 -----------------------------------------------------
 --   WARNING: This file is dynamically generated   --
---   UPDATED: Sunday, May 16, 2021 @ 11:44 AM      --
+--   UPDATED: Sunday, May 16, 2021 @ 10:56 PM      --
 -----------------------------------------------------
 local _ = select(2, ...); local rawset = rawset;
 local g = function(t,g) rawset(t,'g',g); return t; end
